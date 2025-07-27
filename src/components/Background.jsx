03d31d83c1b852891e01e9952c1280bf3cbeac58
Diwangshu Kakoty <diwangshukakoty@gmail.com>
2025-07-27 21:13:07 +0530

    background hide

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,6 +1,24 @@
+import { useState, useEffect, useRef } from "react";
+
 export const Background = () => {
+
+  const [hide, setHide] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      if (scrollY > 500) {
+        setHide(true);
+      } else {
+        setHide(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="background-wrapper">
+    <div className={`background-wrapper ${hide ? "hidden" : ""}`}>
       <div className="scroller">
         <div className="text-strip">
           <h1 className="bloom-text">DIWANGSHU KAKOTY DIWANGSHU KAKOTY</h1>
